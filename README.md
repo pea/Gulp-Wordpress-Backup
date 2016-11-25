5ff31e8794fce5ce7803e11f6f48e5618b9aee2b
Peter Bailey <prodigy.zero@gmail.com>
2016-11-25 17:06:27 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Wordpress_Backup
-Reliable backup gulp tasks for Wordpress 
+# Node Wordpress Backup
+Gulpfile to backup a Wordpress installation for migration.
+
+## Installation
+1. Copy gulpfile.js and package.json to Wordpress root
+2. Execute `npm install`
+
+## Usage
+Run:
+```ssh
+gulp --dbhost <hostname> --dbuser <database username> --dbpass <database password> --dbdatabase <database name>
+```
+
+`/exports` will be created in the Wordpress root containing the backup. The backup will contain an SQL dump and an archive of the files.
+
+To import the backup, create a database with the SQL dump, Configure wp-config.php and other configuration files.
+
+## Commands
+
+### Replace domain in database
+```ssh
+gulp --oldDomain localhost --newDomain mywebsite.com
+```
+
+### Archiver Option
+Choose the archiver to use whem compressing the files. You have the option of zip and tar.gz. Zip can have problems with symlinks. If unzipping is failing use tar.gz.
+```ssh
+gulp --archive zip
+```
+Default: tar.gz
+
+### Exclude Directories
+Exclude a directory. Uses glob syntax.
+
+Exclude all node_modules directories
+```ssh
+gulp --excludeDir **/node_modules
+```
+
+Exclude /node_modules and /wp-content/themes/mytheme/vendor
+```ssh
+gulp --excludeDir node_modules --excludeDir wp-content/themes/mytheme/vendor
+```
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var gulp = require('gulp');
+var mysqlDump = require('mysqldump');
+var datetime = require('node-datetime');
+var dt = datetime.create();
+var mkdirp = require('mkdirp');
+var gulpSequence = require('gulp-sequence');
+var replace = require("gulp-replace");
+var del = require('del');
+var fs = require('fs');
+var zip = require('gulp-zip');
+var gzip = require('gulp-gzip');
+var tar = require('gulp-tar');
+var _ = require('underscore');
+var globule = require('globule');
+
+var argv = require('yargs')
+    .default('oldDomain', '')
+    .default('newDomain', '')
+    .default('archiver', 'tar')
+    .array('excludeDir')
+    .argv
+    
+var dir = dt.format('m-d-y_H.M.S');
+
+RegExp.escape = function(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+/**
+ * Setup directory structure
+ */
+gulp.task('setup', () => {
+    return mkdirp('./exports/' + dir + '/tmp', (err) => {
+        if (err) throw err;
+        fs.writeFile('./exports/.htaccess', 'deny from all', function (err) {
+          if (err) return console.log(err);
+        });
+    });
+});
+
+/**
+ * Archive files, excluding vendors
+ */
+gulp.task('archiveFiles', () => {
+    var srcFiles = [
+        '**',
+        '!./exports/**'
+    ];
+
+    if (!!argv.excludeDir) {
+        _.each(argv.excludeDir, (dir) => {
+
+            var files = globule.find('**/' + dir);
+
+            srcFiles.push(
+                '!' + dir, '!' + dir + '/**'
+            );
+        });
+    }
+
+    if (argv.archiver == 'zip') {
+        return gulp.src(srcFiles, {base: './'})
+            .pipe(zip('files.zip'))
+            .pipe(gulp.dest('./exports/' + dir));
+    }
+
+    if (argv.archiver == 'tar') {
+        return gulp.src(srcFiles)
+            .pipe(tar('files.tar'))
+            .pipe(gzip())
+            .pipe(gulp.dest('./exports/' + dir));
+    }
+});
+
+/**
+ * Dump the database into /tmp, replace the domains and move to /
+ */
+gulp.task('dumpDatabase', () => {
+    var dumpPath = './exports/' + dir + '/tmp/database.sql';
+    return new Promise((resolve,reject) => {
+        mysqlDump({
+            host: argv.dbhost,
+            user: argv.dbuser,
+            password: argv.dbpass,
+            database: argv.dbdatabase,
+            dest: dumpPath 
+        }, (err) => {
+            if(err !== null) return reject(err);
+            resolve(dumpPath);
+        });
+    })
+    .then((err,res) => {
+        if(err !== null);
+        return gulp.src([dumpPath])
+            .pipe(replace(RegExp.escape(argv.oldDomain), argv.newDomain))
+            .pipe(gulp.dest('./exports/' + dir));
+    });
+});
+
+/** 
+ * Delete /temp
+ */
+gulp.task('clearUp', () => {
+    del(['./exports/' + dir + '/tmp/']);
+});
+
+gulp.task('default', gulpSequence(
+    'setup',
+    'archiveFiles',
+    'dumpDatabase',
+    'clearUp'
+));
